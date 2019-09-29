36f7e6912dedb528291dfe89c7af1bb252015dea
Norman Walsh <ndw@nwalsh.com>
2019-09-29 18:25:46 -0500

    Update current and latest to 2.1.0

--- resources/current/js/annotation.js
+++ resources/current/js/annotation.js
@@ -1,16 +1,14 @@
-/* -*- JavaScript -*-
-*/
+// -*- JavaScript -*-
 
-$( document ).ready(function() {
-    $( ".dialog" ).dialog({
-	autoOpen: false,
-	width: "60%"
-    });
+$(document).ready(function() {
+  $(".dialog").dialog({
+    autoOpen: false,
+    width: "60%"
+  });
 
-    $( ".dialog-link" ).click(function( event ) {
-        id = $(this).attr('href').substr(1)
-        console.log("ID:" + id)
-        $( "#" + id ).dialog( "open" );
-	event.preventDefault();
-    });
+  $(".dialog-link").click(function( event ) {
+    id = $(this).attr('href').substr(1)
+    $( "#" + id ).dialog( "open" );
+    event.preventDefault();
+  });
 })
